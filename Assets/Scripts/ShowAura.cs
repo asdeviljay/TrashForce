d5fe0773bfea78618823db75a307aacf2bb55a33
Drink <drink7277@gmail.com>
2018-10-27 11:52:33 +0700

    updted

---
+++ Assets/Scripts/ShowAura.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowAura : MonoBehaviour {
+
+	public slideTrash CompoTrash;
+	Sprite storeImage;
+	SpriteRenderer curSprite;
+	public ParticleSystem ps;
+
+	// Use this for initialization
+	void Start () {
+		//CompoTrash = GetComponentInParent<slideTrash> ();
+		curSprite = GetComponent<SpriteRenderer> ();
+		storeImage = curSprite.sprite;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (CompoTrash.IsClick () && !CompoTrash.OnCollision()) {
+			Vector2 cts = CompoTrash.GetSpriteSize ();
+			curSprite.sprite = storeImage;
+			curSprite.size = new Vector2 (cts.x * 1.05f, cts.y * 1.05f);
+			if (ps.isStopped)
+				ps.Play ();
+		} else {
+			curSprite.sprite = null;
+			ps.Stop ();
+		}
+	}
+}
